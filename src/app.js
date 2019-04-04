67f0f583cccb512a5bc8729050e09b0b03590f94
Lloyd Chambrier <lloydchambrier@gmail.com>
2019-04-04 15:18:10 +0200

    removed unnecessary code & improved readability

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const messagebird = require('messagebird')(`${process.env.MSG_BIRD_KEY}`);
+const messagebird = require('messagebird')(`${process.env.MSG_BIRD_TEST}`);
 const cities = require('cities.json');
 const cron = require('node-cron');
 const Sequelize = require('sequelize')
@@ -86,48 +86,40 @@ const Notification = sequelize.define('notifications', {
 User.hasMany(Notification);
 Notification.belongsTo(User);
 
-// DETECT TIME & FIND MATCHING NOTIFICATIONS
+// DETECT TIME, FIND MATCHING NOTIFICATIONS, FIRE MESSAGE ACCORDINGLY
 cron.schedule('* * * * *', () => {
-let datetime = new Date().timeNow();
-console.log('cron is at work :)')
-console.log(datetime)
-Notification.findAll({where: {notificationTime: datetime}})
-  .then(notifications =>{
-    console.log(JSON.stringify(notifications))
-    if(notifications.length >= 1){
-      // console.log(`Match(es) found ${JSON.stringify(not)}`)
-      notifications.forEach(notification => {
-        const cityLocation = cities.find( city => city.name == notification.city)
-        fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
+      let datetime = new Date().timeNow();
+
+      Notification.findAll({where: {notificationTime: datetime}})
+        .then(notifications => {
+          if(notifications.length >= 1){
+            notifications.forEach(notification => {
+              const cityLocation = cities.find( city => city.name == notification.city)
+              fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
               .then(res => res.json())
-              .then(json =>{
-                // create if (json.code === 400) statement for error checking
-                // console.log('JSON '+ JSON.stringify(json))
-                const temperature = json.currently.temperature
-                const summary = json.currently.summary
-                const daily_data_summary = json.daily.summary
-                messagebird.messages.create({
-                              originator : '+31685765664',
-                              recipients : [ `+31${notification.phone}` ],
-                              body : `${daily_data_summary}`
-                            },
-                            function (err, response) {
-                                  if (err) {
-                                    console.log("ERROR:");
-                                    console.log(err);
-                                } else {
-                                    console.log("SUCCESS:");
-                                    console.log(response);
-                                }
-                            })
-                    }).catch(err => console.error(err))
-      })
-    }
-    else{
-      console.log('No match(es) found :(')
-    }
-  })
-  .catch(err => console.error(err))
+              .then(json => {
+                      const daily_data_summary = json.daily.summary
+                      messagebird.messages.create({
+                                    originator : 'Weather Alerts',
+                                    recipients : [ `+31${notification.phone}` ],
+                                    body : `${daily_data_summary}`
+                                  },
+                                  function (err, response) {
+                                        if (err) {
+                                          console.log(`ERROR: ${err}`);
+                                      } else {
+                                          console.log(`SUCCESS: ${response}`);
+                                      }
+                                  })
+                      })
+                .catch(err => console.error(err))
+              })
+            }
+            else{
+              console.log(`No notifications is scheduled for ${datetime}`)
+            }
+          })
+        .catch(err => console.error(err))
 })
 
 // ROUTES
@@ -142,12 +134,9 @@ app.get('/signup', (req, res)=>{
 app.post('/signup', (req, res) =>{
   const cityName = req.body.city;
   const cityLocation = cities.find( city => city.name == cityName)
-  //console.log(`City match found ${JSON.stringify(cities.find( city => city.name == cityName))}`);
   
   const inputpassword = req.body.password
   const saltRounds = 11
-  console.log(`req.body ${JSON.stringify(req.body)}`)
-  console.log(`password ${inputpassword}`)
 
     bcrypt.hash(inputpassword, saltRounds).then(hash => {
         User.create({
@@ -161,9 +150,7 @@ app.post('/signup', (req, res) =>{
           fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
               .then(res => res.json())
               .then(json =>{
-                // create if (json.code === 400) statement for error checking
                 req.session.user = user;
-                // console.log('JSON '+ JSON.stringify(json))
                 const temperature = json.currently.temperature
                 const summary = json.currently.summary
                 const daily_data_summary = json.daily.summary
@@ -178,53 +165,47 @@ app.post('/signup', (req, res) =>{
 app.post('/login', (req, res) => {
     const email = req.body.email
     const password = req.body.password
-
-    console.log(req.body)
-    // input validation
-    if (password == null || password.length < 8 ||
+  
+    if (password == null || password.length < 8 || // data validation
         email == null || email.length == 0) {
         res.render('index', { loginFailed: true })
         return;
     }
-    //user authentication
-    User.findOne({
-        where: {
-            email: email
-        }
-    }).then((user) => {
-        if (user == null) {
-            res.render('index', { loginFailed: true })
-        } else {
-        bcrypt.compare(password, user.password)
-            .then((result) => {
-                if (result) {
-                    const cityLocation = cities.find( city => city.name == user.city)
-                    req.session.user = user;
-                    fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
-                    .then(res => res.json())
-                    .then(json =>{
-                      // create if (json.code === 400) statement for error checking
-                      req.session.user = user;
-                      // console.log(`JSON ${JSON.stringify(json.currently)}`)
-                      const temperature = json.currently.temperature
-                      const summary = json.currently.summary
-                      const daily_data_summary = json.daily.summary
-                      const date = new Date(new Date().getTime())
-                      res.render('profile', {user: user, temperature: temperature, summary: summary, expect: daily_data_summary, date: date, id: req.session.user.id})
-                    }).catch( err => console.error(err))
-                } else {
-                    res.render('index', { loginFailed: true });
-                }
-            })
-            .catch((err) => {
-                console.log(err, err.stack)
-              })
-        }
+  
+    User.findOne({where: {email: email}})
+        .then( user => {
+            if (user == null) {
+                res.render('index', { loginFailed: true })
+            } else {
+                bcrypt.compare(password, user.password)
+                    .then( result => {
+                        if (result) {
+                            const cityLocation = cities.find( city => city.name == user.city)
+                            req.session.user = user;
+                            fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
+                            .then(res => res.json())
+                            .then(json =>{
+                              req.session.user = user;
+                              const temperature = json.currently.temperature
+                              const summary = json.currently.summary
+                              const daily_data_summary = json.daily.summary
+                              const date = new Date(new Date().getTime())
+                              res.render('profile', {user: user, temperature: temperature, summary: summary, expect: daily_data_summary, date: date, id: req.session.user.id})})
+                            .catch( err => console.error(err))
+                            } 
+                        else {
+                            res.render('index', { loginFailed: true })
+                        }
+                      })
+                    .catch((err) => {
+                        console.log(err, err.stack)
+                      })
+                  }
 
-    }).catch((err) => {
-        console.log(err, err.stack)
-        res.render('index', { loginFailed: true })
-    })
+            }).catch((err) => {
+                console.log(err, err.stack)
+                res.render('index', { loginFailed: true })
+            })
 })
 
 app.get('/logout', (req, res) => {
@@ -237,53 +218,46 @@ app.get('/logout', (req, res) => {
 })
 
 app.get('/profile', (req, res)=>{
-  if(req.session.user){
-    const cityLocation = cities.find( city => city.name == req.session.user.city)
-    fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
-      .then(res => res.json())
-      .then(json =>{
-        const temperature = json.currently.temperature
-        const summary = json.currently.summary
-        const daily_data_summary = json.daily.summary
-        const date = new Date(new Date().getTime())
-        res.render('profile', {user: req.session.user, temperature: temperature, summary: summary, expect: daily_data_summary, date: date, id: req.session.user.id})
-     })
-   }
-   else{
-    res.render('index', { loginFailed: true });
-   }
+      if(req.session.user){
+        const cityLocation = cities.find( city => city.name == req.session.user.city)
+        fetch(`https://api.darksky.net/forecast/${process.env.DARK_SKY_KEY}/${cityLocation.lat},${cityLocation.lng}?units=si`)
+          .then(res => res.json())
+          .then(json =>{
+            const temperature = json.currently.temperature
+            const summary = json.currently.summary
+            const daily_data_summary = json.daily.summary
+            const date = new Date(new Date().getTime())
+            res.render('profile', {user: req.session.user, temperature: temperature, summary: summary, expect: daily_data_summary, date: date, id: req.session.user.id})
+        })
+      }
+      else{
+        res.render('index', { loginFailed: true });
+      }
 })
 
 app.get('/add-notification', (req, res)=>{
-  console.log(`req.session ${JSON.stringify(req.session)}`)
-  res.render('add-notification')
+    res.render('add-notification')
 })
 app.post('/notifications', (req, res)=>{
-  console.log(`req.body ${JSON.stringify(req.body)}`)
     Notification.create({
       phone: req.body.phone,
       notificationTime: req.body.time,
       city: req.body.city,
       userId: req.session.user.id
     })
-    .then(()=>{
-      res.redirect(`/profile`)
-    })
+    .then(() => res.redirect(`/profile`))
     .catch(err => console.error(err))
 })
 app.get('/notifications', (req, res)=>{
-  Notification.findAll()
-  .then(notifications=>{
-    res.render('notifications', {notifications: notifications})
-  })
-  .catch(err => console.error(err))
+    Notification.findAll()
+    .then(notifications => res.render('notifications', {notifications: notifications}))
+    .catch(err => console.error(err))
 })
 
 sequelize.sync()
 .then(()=>{
   app.listen(port, ()=>{
     let datetime = new Date().timeNow();
-    console.log(`App is listening on port ${port}`)
-    console.log(`running a task every minute, current time is ${datetime}`)
+    console.log(`App is listening on port ${port}, current time is ${datetime}`)
   })
 })
--- src/timer.js
+++ src/timer.js
@@ -1,27 +1,11 @@
-var cron = require('node-cron');
-
-// For todays date;
-Date.prototype.today = function () { 
-    return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
-}
+// module.exports = { today: function () { 
+//     return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+// }}
 // For the time now
-Date.prototype.timeNow = function(){ 
-    return ((this.getHours() < 10)?"0":"") + ((this.getHours()>12)?(this.getHours()-12):this.getHours()) +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds() + ((this.getHours()>12)?('PM'):'AM'); 
-};
+// exports.timeNow = function(){ 
+//     return ((this.getHours() < 10)?"0":"") + ((this.getHours()>12)?(this.getHours()-12):this.getHours()) +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds() + ((this.getHours()>12)?('PM'):'AM'); 
+// };
 
-cron.schedule('* * * * * *', () => {
-    var datetime = new Date().today() + " @ " + new Date().timeNow();
-    console.log(`running a task every second, current time is ${datetime}`);
-    
-  });
-
-  cron.schedule('* * * * *', () => {
-    console.log(`running a task every minute, current time is ${datetime}`)
-    // query the database for any notification time that matches current time
-    // if match(es) found
-    //    send a notification text to every match(es)
-
-    // ! Make sure that the format used for stored "notification time"
-    // is the same format used for "current time"
-    // ! 
-  });
+exports.sayHelloInEnglish = function() {
+  return "HELLO";
+  };
